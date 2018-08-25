d4388a5a73db65170dca17365ce2696c0b2f09ea
MohamedSherif94 <mohammed.said4368@el-eng.menofia.edu.eg>
2018-08-25 05:22:51 +0200

    Initial commit(Adding Tabs)

---
+++ app/src/main/java/com/example/iba/iba/MainActivity.java
@@ -0,0 +1,87 @@
+package com.example.iba.iba;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.iba.iba.R;
+import com.example.iba.iba.fragments.ClientsFragment;
+import com.example.iba.iba.fragments.GiftsFragment;
+import com.example.iba.iba.fragments.HomeFragment;
+import com.example.iba.iba.fragments.AboutUsFragment;
+import com.example.iba.iba.fragments.PresidentWordFragment;
+import com.example.iba.iba.fragments.ServicesAndProductsFragment;
+import com.example.iba.iba.fragments.StaffFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new HomeFragment(), "HOME");
+        adapter.addFragment(new AboutUsFragment(), "ABOUTUS");
+        adapter.addFragment(new PresidentWordFragment(), "PRESIDENT");
+        adapter.addFragment(new ServicesAndProductsFragment(), "SERVICES & PRODUCTS");
+        adapter.addFragment(new StaffFragment(), "STAFF");
+        adapter.addFragment(new ClientsFragment(), "CLIENTS");
+        adapter.addFragment(new GiftsFragment(), "GIFTS");
+        viewPager.setAdapter(adapter);
+    }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/iba/iba/fragments/StaffFragment.java
@@ -0,0 +1,31 @@
+package com.example.iba.iba.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.iba.iba.R;
+
+
+public class StaffFragment extends Fragment {
+
+    public StaffFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_staff, container, false);
+    }
+
+}
